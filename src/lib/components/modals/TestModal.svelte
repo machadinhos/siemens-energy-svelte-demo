2468cae9bb43249c6a436ef34912ad497c5fce56
filipe-silva <filipe.silva@siemens-energy.com>
2026-01-22 21:20:38 +0000

    style: add some CSS to Projects list page

--- src/lib/components/modals/TestModal.svelte
+++ src/lib/components/modals/TestModal.svelte
@@ -22,6 +22,35 @@
   };
 </script>
 
-<input type="text" bind:value={projectName} />
-<button onclick={save} type="button">test</button>
-{text}
+<div class="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+  <div class="sm:flex sm:items-start">
+    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+      <h3 id="dialog-title" class="text-base font-semibold text-white">Create Project</h3>
+      <div class="mt-2">
+        <p class="text-sm text-gray-400">Insert here the name of the new project.</p>
+        <div class="mt-2">
+          <input
+            type="text"
+            bind:value={projectName}
+            class="w-full rounded-md border border-gray-300 bg-gray-700 px-3 py-2 text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
+            placeholder="Project Name"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+  <button
+    onclick={save}
+    type="button"
+    class="inline-flex w-full justify-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white hover:bg-red-400 sm:ml-3 sm:w-auto"
+    >Save</button
+  >
+  <button
+    onclick={() => modal.close()}
+    type="button"
+    class="mt-3 inline-flex w-full justify-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20 sm:mt-0 sm:w-auto"
+    >Cancel</button
+  >
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,25 +10,54 @@
   let { data }: PageProps = $props();
 </script>
 
-<button class="block cursor-pointer" onclick={openModal} type="button">test modal</button>
+<button
+  class="my-2 flex items-center gap-3 rounded-lg bg-white px-4 py-2 text-center align-middle font-sans text-xs font-bold text-black uppercase shadow-md shadow-gray-900/10 transition-all select-none hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+  onclick={openModal}
+  type="button"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    aria-hidden="true"
+    stroke-width="2"
+    class="h-4 w-4"
+  >
+    <path
+      d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"
+    >
+    </path>
+  </svg>
+  Add Project
+</button>
 
-<table>
-  <thead>
-    <tr>
-      <th>Id</th>
-      <th>Name</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each data.projects as project (project.id)}
+<div
+  class="relative flex h-auto w-auto flex-col overflow-scroll rounded-lg bg-white bg-clip-border text-gray-700 shadow-md"
+>
+  <table class="w-full min-w-max table-auto text-left">
+    <thead>
       <tr>
-        <td>
-          <a href="/project/{project.id}">{project.id}</a>
-        </td>
-        <td>
-          {project.name}
-        </td>
+        <th class="border-b border-slate-300 bg-slate-50 p-4">
+          <p class="block text-sm leading-none font-normal text-slate-500">Id</p>
+        </th>
+        <th class="border-b border-slate-300 bg-slate-50 p-4">
+          <p class="block text-sm leading-none font-normal text-slate-500">Name</p>
+        </th>
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      {#each data.projects as project (project.id)}
+        <tr class="hover:bg-slate-50">
+          <td class="border-b border-slate-200 p-4">
+            <p class="block text-sm text-slate-800">{project.id}</p>
+          </td>
+          <td class="border-b border-slate-200 p-4">
+            <a href="/project/{project.id}" class="block text-sm font-semibold text-slate-800">
+              {project.name}
+            </a>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
